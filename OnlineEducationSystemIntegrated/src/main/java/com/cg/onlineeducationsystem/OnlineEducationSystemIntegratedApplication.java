8a703f456710d590993f390cd274b5ece68090eb
Ruchi <Ruchi@DESKTOP-897ML7P>
2021-01-17 23:27:13 +0530

    First commit

---
+++ OnlineEducationSystemIntegrated/src/main/java/com/cg/onlineeducationsystem/OnlineEducationSystemIntegratedApplication.java
@@ -0,0 +1,13 @@
+package com.cg.onlineeducationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEducationSystemIntegratedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEducationSystemIntegratedApplication.class, args);
+	}
+
+}
---
+++ OnlineEducationSystemIntegrated/src/test/java/com/cg/onlineeducationsystem/OnlineEducationSystemIntegratedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.onlineeducationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEducationSystemIntegratedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
